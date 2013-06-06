e9c32fec2abf700144b5af8f4144807801e74351
lontoken <lontoken@gmail.com>
2013-06-07 00:31:05 +0800

    README修改提交；slice.h注释提交；skip lists论文出处提交；

--- leveldb-1.10.0/include/leveldb/slice.h
+++ leveldb-1.10.0/include/leveldb/slice.h
@@ -4,11 +4,11 @@
 //
 // Slice is a simple structure containing a pointer into some external
 // storage and a size.  The user of a Slice must ensure that the slice
-// is not used after the corresponding external storage has been
-// deallocated.
+// is not used after the corresponding相应的 external storage has been
+// deallocated释放.
 //
 // Multiple threads can invoke const methods on a Slice without
-// external synchronization, but if any of the threads may call a
+// external外部的 synchronization 同步, but if any of the threads may call a
 // non-const method, all threads accessing the same Slice must use
 // external synchronization.
 
